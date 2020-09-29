baa8136245ad192f84d21fa112991832eaadb31a
Mateusz Nejman <mateusz_nejman@hotmail.com>
2020-09-29 13:03:36 +0200

    Debug Layer

--- Neno/ApplicationConfig.cpp
+++ Neno/ApplicationConfig.cpp
@@ -2,10 +2,11 @@
 
 namespace neno
 {
-	ApplicationConfig::ApplicationConfig(int screenWidth, int screenHeight, bool allowFullscreen, Color clearBufferColor) {
+	ApplicationConfig::ApplicationConfig(int screenWidth, int screenHeight, bool allowFullscreen, bool debugMode, Color clearBufferColor) {
 		this->screenWidth = screenWidth;
 		this->screenHeight = screenHeight;
 		this->allowFullscreen = allowFullscreen;
 		this->clearBufferColor = clearBufferColor;
+		this->debugMode = debugMode;
 	}
 }
